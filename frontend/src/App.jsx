74bb36576e52245b1bbc1e6e34730770c138f415
Mudita Jain <mudita21jain@gmail.com>
2025-03-26 13:32:03 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import MapPage from "./pages/MapPage"; // Import Map Page
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/map" element={<MapPage />} /> {/* NEW ROUTE */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
